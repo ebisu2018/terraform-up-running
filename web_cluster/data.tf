f20ee152d888f6e62a96a393e074780c3e3282d2
Zayed <919357104@qq.com>
2025-02-18 10:54:24 +0800

    update

--- web_cluster/data.tf
+++ web_cluster/data.tf
@@ -9,20 +9,16 @@ data "aws_subnets" "default" {
   }
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
-  config = {
-    region = "ap-northeast-1"
-    bucket = var.db_remote_state_bucket
-    key = var.db_remote_state_key
-  }
-}
+# data "terraform_remote_state" "db" {
+#   backend = "s3"
+#   config = {
+#     region = "ap-northeast-1"
+#     bucket = var.db_remote_state_bucket
+#     key = var.db_remote_state_key
+#   }
+# }
 
 data "template_file" "user-data" {
   template = file(var.user_data_script)
-  vars = {
-    server_port = local.http_port
-    db_address = data.terraform_remote_state.db.outputs.address
-    db_port = data.terraform_remote_state.db.outputs.port
-  }
+  vars = {}
 }
--- web_cluster/var.tf
+++ web_cluster/var.tf
@@ -10,13 +10,13 @@ variable "cluster_name" {
   type = string
 }
 
-variable "db_remote_state_bucket" {
-  type = string
-}
-
-variable "db_remote_state_key" {
-  type = string
-}
+# variable "db_remote_state_bucket" {
+#   type = string
+# }
+#
+# variable "db_remote_state_key" {
+#   type = string
+# }
 
 variable "min_size" {
   type = number
