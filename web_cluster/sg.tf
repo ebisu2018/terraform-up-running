ccef92e08a21ec353d83f83466ecd282ca0da931
Zayed <919357104@qq.com>
2025-02-18 10:26:16 +0800

    Update sg.tf

--- web_cluster/sg.tf
+++ web_cluster/sg.tf
@@ -9,23 +9,12 @@ resource "aws_security_group_rule" "asg_rule" {
   security_group_id = aws_security_group.instance-sg.id
   to_port           = local.any_port
   type              = "ingress"
+  cidr_blocks = local.all_ips
 }
 
 
 resource "aws_security_group" "alb-sg" {
-  name = "${var.cluster_name}-alb-sg"
-#   ingress {
-#     from_port = local.any_port
-#     to_port = local.any_port
-#     protocol = local.tcp_protocol
-#     cidr_blocks = local.all_ips
-#   }
-#   egress {
-#     from_port = local.any_port
-#     to_port = local.any_port
-#     protocol = local.any_protocol
-#     cidr_blocks = local.all_ips
-#   }
+  name = "${var.cluster_name}-alb-sg-v2"
 }
 
 
