4dc445b374cb324a4c1883ba5133588bfdffeee6
Zayed <919357104@qq.com>
2025-03-30 08:48:50 +0800

    update

--- v2/cluster/asg-rolling-deploy/vars.tf
+++ v2/cluster/asg-rolling-deploy/vars.tf
@@ -10,14 +10,6 @@ variable "cluster_name" {
   type = string
 }
 
-# variable "db_remote_state_bucket" {
-#   type = string
-# }
-#
-# variable "db_remote_state_key" {
-#   type = string
-# }
-
 variable "min_size" {
   type = number
 }
